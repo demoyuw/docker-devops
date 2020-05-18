d06982e6e2a2bd71c5ca76b71115bce8a816e7de
YuWei Chou <demoyuw@gmail.com>
2020-05-18 20:23:19 +0800

    Add create vm script.

---
+++ for-create-vm.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+for ((i=38; i<=40; i++))
+do
+    gcloud compute --project=k8s-cluster3-276411 instances create k8s-vm1-$i \
+    --zone=us-west1-b --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM \
+    --maintenance-policy=MIGRATE --service-account=478327229581-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+    --tags=http-server,https-server --image=ubuntu-1604-xenial-v20200429 --image-project=ubuntu-os-cloud \
+    --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=k8s-vm1-$i
+done
