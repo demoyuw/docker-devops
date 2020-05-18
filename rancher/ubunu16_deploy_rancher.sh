b0301903163d032d2c46f5231d1f10865e776116
YuWei Chou <demoyuw@gmail.com>
2020-05-18 19:43:20 +0800

    新增deploy docker and rancher script.

---
+++ rancher/ubunu16_deploy_rancher.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+WORK_DIR=/home/demoyuw
+LOGFILE=$WORK_DIR/log/rancher_install.log
+mkdir $WORK_DIR/log
+touch $LOGFILE
+
+apt-get update -y &>> $LOGFILE
+apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common  -y &>> $LOGFILE
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" &>> $LOGFILE
+apt-get update -y &>> $LOGFILE 
+apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial -y &>> $LOGFILE
+usermod -aG docker demoyuw &>> $LOGFILE
+
+mkdir $WORK_DIR/rancher &>> $LOGFILE
+chmod 755 $WORK_DIR/rancher &>> $LOGFILE
+docker run -d --restart=unless-stopped -p 80:80 -p 443:443 -v $WORK_DIR/rancher:/var/lib/rancher rancher/rancher &>> $LOGFILE
---
+++ rancher/ubunu16_install_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WORK_DIR=/home/demoyuw
+LOGFILE=$WORK_DIR/log/docker_install.log
+mkdir $WORK_DIR/log
+touch $LOGFILE
+
+apt-get update -y &>> $LOGFILE
+apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common -y &>> $LOGFILE
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" &>> $LOGFILE
+apt-get update -y &>> $LOGFILE 
+apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial -y &>> $LOGFILE
+usermod -aG docker demoyuw &>> $LOGFILE
