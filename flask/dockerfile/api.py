18399e80ea8589ed44f7bb1f0667bb66e1937661
YuWei Chou <demoyuw@gmail.com>
2020-05-20 07:23:05 +0800

    update files

---
+++ flask/dockerfile/api.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "flask api v2 is working"
+
+
+if __name__ == "__main__":
+    # Get Parameter
+    app.run(host='0.0.0.0', port=8080)
