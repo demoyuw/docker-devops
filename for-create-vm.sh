ae859af689c0e3b7d565e7c61f319380b2246665
YuWei Chou <demoyuw@gmail.com>
2022-06-28 01:30:36 +0800

    add sonarqube

--- for-create-vm.sh
+++ for-create-vm.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-for ((i=47; i<=48; i++))
+for ((i=9; i<=12; i++))
 do
-    gcloud compute --project=turnkey-cocoa-285018 instances create docker-devops-vm1-$i \
-    --zone=us-east1-b --machine-type=e2-standard-2 --subnet=default --network-tier=PREMIUM \
-    --maintenance-policy=MIGRATE --service-account=177980045088-compute@developer.gserviceaccount.com \
+    gcloud compute --project=cedar-card-352414 instances create gitlab-cicd-vm2-$i \
+    --zone=us-west1-b --machine-type=e2-standard-2 --subnet=default --network-tier=PREMIUM \
+    --maintenance-policy=MIGRATE --service-account=293756706705-compute@developer.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
-    --tags=http-server,https-server --image=ubuntu-1604-xenial-v20210416 --image-project=ubuntu-os-cloud \
-    --boot-disk-size=20GB --boot-disk-type=pd-standard --boot-disk-device-name=docker-devops-vm2-$i
+    --tags=http-server,https-server --image=ubuntu-2004-focal-v20220615 --image-project=ubuntu-os-cloud \
+    --boot-disk-size=40GB --boot-disk-type=pd-standard --boot-disk-device-name=gitlab-cicd-vm2-$i
 done
