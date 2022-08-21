9c6d013228f32b0d79ed4db4911887fc98f6995a
YuWei Chou <demoyuw@gmail.com>
2022-08-21 12:34:25 +0800

    update shell

--- for-create-vm.sh
+++ for-create-vm.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-for ((i=9; i<=12; i++))
+for ((i=9; i<=10; i++))
 do
-    gcloud compute --project=cedar-card-352414 instances create gitlab-cicd-vm2-$i \
+    gcloud compute --project=cedar-card-352414 instances create devops-cicd-vm2-$i \
     --zone=us-west1-b --machine-type=e2-standard-2 --subnet=default --network-tier=PREMIUM \
     --maintenance-policy=MIGRATE --service-account=293756706705-compute@developer.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
-    --tags=http-server,https-server --image=ubuntu-2004-focal-v20220615 --image-project=ubuntu-os-cloud \
-    --boot-disk-size=40GB --boot-disk-type=pd-standard --boot-disk-device-name=gitlab-cicd-vm2-$i
+    --tags=http-server,https-server --image=ubuntu-2004-focal-v20220701 --image-project=ubuntu-os-cloud \
+    --boot-disk-size=40GB --boot-disk-type=pd-standard --boot-disk-device-name=devops-cicd-vm2-$i
 done
