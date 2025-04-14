179692e54b17b17f4e7ef1136eea362bcff5269c
demoyuw <demoyuw@gmail.com>
2025-04-14 22:55:54 +0800

    Update install rancher script

--- script/ubuntu_deploy_rancher.sh
+++ script/ubuntu_deploy_rancher.sh
@@ -2,20 +2,6 @@
 
 WORK_DIR=/home/`logname`
 
-apt-get update
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-usermod -aG docker `logname`
-newgrp docker
-
 mkdir -p $WORK_DIR/rancher
 chmod 755 $WORK_DIR/rancher
 docker run -d --privileged --restart=unless-stopped -p 10080:80 -p 10443:443 -v $WORK_DIR/rancher:/var/lib/rancher rancher/rancher:stable
