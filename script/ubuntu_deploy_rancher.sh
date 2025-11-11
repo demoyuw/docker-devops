069bcc9ff838772aa7d6e5cb5eda7f0097b579e9
demoyuw <demoyuw@gmail.com>
2025-11-11 21:35:46 +0800

    set rancher version v2.10.3

--- script/ubuntu_deploy_rancher.sh
+++ script/ubuntu_deploy_rancher.sh
@@ -4,4 +4,4 @@ WORK_DIR=/home/`logname`
 
 mkdir -p $WORK_DIR/rancher
 chmod 755 $WORK_DIR/rancher
-docker run -d --privileged --restart=unless-stopped -p 10080:80 -p 10443:443 -v $WORK_DIR/rancher:/var/lib/rancher rancher/rancher:stable
+docker run -d --privileged --restart=unless-stopped -p 10080:80 -p 10443:443 -v $WORK_DIR/rancher:/var/lib/rancher rancher/rancher:v2.10.3
